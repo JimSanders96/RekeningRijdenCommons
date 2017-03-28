0852f2db99935db2b72c5ccfa274df3ba8161c69
Jim Sanders <j.sanders@student.fontys.nl>
2017-03-28 10:28:32 +0200

    Added an empty class to test if this fixes jitpack issues.

---
+++ src/main/java/domain/EmptyClass.java
@@ -0,0 +1,20 @@
+package domain;
+
+/**
+ * Created by Jim on 28-3-2017.
+ */
+public class EmptyClass {
+    private int test;
+
+    public EmptyClass(int test) {
+        this.test = test;
+    }
+
+    public int getTest() {
+        return test;
+    }
+
+    public void setTest(int test) {
+        this.test = test;
+    }
+}
